4a5fe7ccd224867d13678f273bc4485ae5a5d90e
Gino Cortes <gino.angelo.cortes@gmail.com>
2016-07-08 01:18:48 -0400

    basic functionality including commenting working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,19 +12,39 @@ class CommentsController < ApplicationController
   end
 
   def create
-      # @user = User.find(session[:user_id])
-      # @post = Post.find(params[:id])
-		  @comment = Comment.create(comment_params)
-        # body: params[:post][:body],
-        # post_id: session[:post_id],
-        # user_id: session[:user_id]
-        # )
-      flash[:notice] = "You have commented."
-      redirect_to "/posts"
-  #   else 
-  #     render "posts"
-		# end
+    # find a post by the foreign key post id
+    @post = Post.find(params[:post_id])
+    # define comment with the params we created
+    @comment = @post.comments.create(params[:comment].permit(:body))
+
+    redirect_to post_path(@post)
   end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
+
+  end
+
+
+
+  # def create
+  #     # @user = User.find(session[:user_id])
+  #     # @post = Post.find(params[:id])
+		#   @comment = Comment.create(comment_params)
+  #       # body: params[:post][:body],
+  #       # post_id: session[:post_id],
+  #       # user_id: session[:user_id]
+  #       # )
+  #     flash[:notice] = "You have commented."
+  #     redirect_to "/posts"
+  # #   else 
+  # #     render "posts"
+		# # end
+  # end
 
   private 
   def comment_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,36 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  # get 'posts/index'
 
-  get 'posts/new'
+  # get 'posts/new'
 
-  get 'posts/edit'
+  # get 'posts/edit'
 
-  get 'users/index'
+  # get 'users/index'
 
-  get 'users/users'
+  # get 'users/users'
 
-  get 'users/post'
+  # get 'users/post'
 
-  get 'users/comment'
+  # get 'users/comment'
 
   get 'users/signinform' => 'users#signinform'
   post 'users/signin' => 'users#signin'
 
-  get 'users/edit/:id' => 'users#edit'
+  # get 'users/edit/:id' => 'users#edit'
 
-  get 'posts/edit/:id' => 'posts#edit'
+  # get 'posts/edit/:id' => 'posts#edit'
 
   get 'users/signout' => 'users#signout'
   post 'users/signout' => 'users#signout'
 
-  get '/comments' => 'comments#new'
-  post '/comments' => 'comments#create'
+  # get '/comments' => 'comments#new'
+  # post '/comments' => 'comments#create'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, :posts, :comments
-
+  resources :users
+  resources :posts do
+    resources :comments
+  end
+  
   root 'users#index'
 end
