e0f860c162dc1792417b4719666fa050687f6306
Gino Cortes <gino.angelo.cortes@gmail.com>
2016-07-11 12:36:00 -0400

    use of helpers to create link to new tab for real TUmblr

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
 	def fancy_title(title)
 		# "this is the fancy " + title
-		content_tag :h1, title
+		content_tag :h1, title #h1 tag created
+	end
+
+	def tumblr_anchor
+		link_to "Real Tumblr", "http://www.tumblr.com", :target => "_blank" #opens in new tag
 	end
 end
