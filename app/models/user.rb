438aaa96e5dcdcfaae7284e2710ec3b0bb77466d
Gino Cortes <gino.angelo.cortes@gmail.com>
2016-07-13 19:44:59 -0400

    changed text_field to password_field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	has_many :posts
 	has_many :comments
-	validates :name, :presence => true 
+	validates_presence_of :name 
 	validates :age, :presence => true, :numericality => {:only_integer => true}
 	validates :password, :presence => true, :length => { minimum: 4 }
 end
